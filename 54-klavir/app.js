fc558df6e161685a3df81d22f2ddb740b8481d2c
AlenS <alenskupnjak@yahoo.com>
2021-01-20 07:05:44 +0100

    073 dodan zvuk projekt-54

--- 54-klavir/app.js
+++ 54-klavir/app.js
@@ -1,37 +1,57 @@
-'use strict'
+'use strict';
 
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
-function removeTransition (e) {
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+
+  btn.addEventListener('click', (btn) => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+  document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((zvuk) => {
+    const pjesma = document.getElementById(zvuk);
+    pjesma.currentTime = 0;
+    pjesma.pause();
+  });
+}
+
+// ====================================================================
+// Klavir
+function removeTransition(e) {
   console.log(e);
   // 1 nacin
   // e.target.classList.remove('playing')
 
   // 2 nacin
-  this.classList.remove('playing')
-  
+  this.classList.remove('playing');
 }
-
 
 // želimo selektirati sve tipke
-const sveTipke = document.querySelectorAll('.tipka')
-sveTipke.forEach(tipka => {
-  tipka.addEventListener('transitionend', removeTransition)
-})
+const sveTipke = document.querySelectorAll('.tipka');
+sveTipke.forEach((tipka) => {
+  tipka.addEventListener('transitionend', removeTransition);
+});
 
-
-
-
-function sviraj (e) {
-  const odabranaMuzika = document.querySelector(`audio[data-tipka="${e.keyCode}"]`)
-  const stisnutaTipka = document.querySelector(`div[data-tipka="${e.keyCode}"]`)
-  if (!odabranaMuzika) return
+function sviraj(e) {
+  const odabranaMuzika = document.querySelector(
+    `audio[data-tipka="${e.keyCode}"]`
+  );
+  const stisnutaTipka = document.querySelector(
+    `div[data-tipka="${e.keyCode}"]`
+  );
+  if (!odabranaMuzika) return;
   console.log(odabranaMuzika, stisnutaTipka);
   odabranaMuzika.currentTime = 0;
-  odabranaMuzika.play()
+  odabranaMuzika.play();
 
-  stisnutaTipka.classList.add('playing')
+  stisnutaTipka.classList.add('playing');
 }
 
-
-window.addEventListener('keydown', sviraj)
-
+window.addEventListener('keydown', sviraj);
