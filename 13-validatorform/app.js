f32f898eb25fc35e11958591e769c569fa8ed195
AlenS <alenskupnjak@yahoo.com>
2021-02-09 09:55:29 +0100

    086 password generator

--- 13-validatorform/app.js
+++ 13-validatorform/app.js
@@ -1,3 +1,134 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+console.log(randomFunc.lower);
+
+// kopiranje na clipboard
+clipboardEl.addEventListener('click', (e)=> {
+  const textarea = document.createElement('textarea')
+  const password = resultEl.innerText
+
+  if (!password) {
+    return
+  }
+  textarea.value = password
+  document.body.appendChild(textarea)
+  textarea.select()
+  document.execCommand('copy')
+  textarea.remove()
+  alert('Password copied to clipboard =' + password)
+
+})
+
+
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+// Generiranje passworda
+generateEl.addEventListener('click', (e) => {
+  const lengthPassword = +lengthEl.value; // znak + pretvara broj u number!!
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  resultEl.innerHTML = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    lengthPassword
+  );
+});
+
+function generatePassword(lower, upper, number, symbol, lengthPassword) {
+  let generPassword = '';
+  let generPasswordMoj = '';
+
+  const typesCount = lower + upper + number + symbol;
+  const typesAtrray = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (data) => {
+      // console.log(Object.values(data));
+      // console.log(Object.values(data)[0]);
+
+      return Object.values(data)[0] === true;
+    }
+  );
+
+  console.log(typesAtrray);
+
+  // ako nista nije selektirano izlazi iz petlje
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for (let index = 0; index < lengthPassword; index++) {
+    if (lower === true) {
+      generPasswordMoj += getRandomLower();
+    }
+    if (upper === true) {
+      generPasswordMoj += getRandomUpper();
+    }
+    if (number === true) {
+      generPasswordMoj += getRandomNumber();
+    }
+    if (symbol === true) {
+      generPasswordMoj += getRandomSymbol();
+    }
+  }
+
+  generPasswordMoj = generPasswordMoj.slice(0,lengthPassword)
+  console.log(generPasswordMoj);
+
+  for (let i = 0; i < lengthPassword; i += typesCount) {
+    console.log(i);
+    typesAtrray.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      console.log(funcName);
+
+      generPassword += randomFunc[funcName]();
+    });
+  }
+
+  // dovivamo maksimalnu duljinu sloga
+  const finalPassword = generPassword.slice(0, lengthPassword);
+  return finalPassword;
+}
+
+
+
+// 
+//
+// ==============================================
+// donja forma ***************************
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
